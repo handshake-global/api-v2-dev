d18bb899312b7d8dcda931d9701b8260fbd722e9
Rudra <>
2020-08-08 15:18:22 +0530

    Update Card_model.php

--- application/models/Card_model.php
+++ application/models/Card_model.php
@@ -58,9 +58,9 @@ class Card_model extends CI_Model {
 									card_config ,card
 									WHERE card.userId in (".$data['userId'].") and card.cardId = card_config.cardId and card.addedMode != 4 and isDefault = 1
 									group by card.cardId, card_config.side
-									limit 1
 							") 
 						->result();
+				 		
 				$real_card = array();
 				$cards_array = json_decode(json_encode($cards), true);
 
