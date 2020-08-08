29507d783fb356d26a42e778fb8982de02f916a2
Rudra <>
2020-08-08 14:29:29 +0530

    Update Card_model.php

--- application/models/Card_model.php
+++ application/models/Card_model.php
@@ -113,6 +113,8 @@ class Card_model extends CI_Model {
 			}
 		$real_card = $mutualsContacts = array();
 		$cards_array = json_decode(json_encode($cards), true);
+		pr($cards_array);
+		exit;
 		foreach($cards as $card):
 			if($card->side==1)
 				$real_card[$card->cardId][] = array('frontImage' =>$card->cardImage,'frontVideo'=>$card->cardVideo,'frontVideoThumbnail'=>ltrim($card->videoThumbnail,'.'));
