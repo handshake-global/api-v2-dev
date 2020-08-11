f55b159cf099f173414fb7fc59fdceac49618770
Rudra <>
2020-08-11 14:00:27 +0530

    Update Card_model.php

--- application/models/Card_model.php
+++ application/models/Card_model.php
@@ -245,6 +245,7 @@ class Card_model extends CI_Model {
 						$mutualsContacts[$value] =$t;
 						$temp[] = $t;
 					}
+					pr($temp);
 					$temp = array_reduce($temp, function($last, $row) {
 		                return $last + $row;
 		            }, array());
