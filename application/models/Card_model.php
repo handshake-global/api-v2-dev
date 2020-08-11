8b980f39dc0cd737aa09d72c51e5449a92350527
Rudra <>
2020-08-11 14:51:35 +0530

    Update Card_model.php

--- application/models/Card_model.php
+++ application/models/Card_model.php
@@ -269,7 +269,7 @@ class Card_model extends CI_Model {
 										$single = array_filter($users,function($vl,$kk) use ($search){
 												  return $vl['userId'] == $search;
 												},ARRAY_FILTER_USE_BOTH);
-										if(isest($single[0]))
+										if(isset($single[0]))
 											$value['mutuals'][] = $single[0];
 									}
 							}
